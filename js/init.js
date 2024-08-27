ad49e779393bb3f21146c96d3a80ab1b39ebf4c7
UAACadmin <admin@uaac-aauc.com>
2024-08-27 16:20:32 -0400

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+	$(document).ready(function(){
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+    $('.modal').modal();
+  });
+  
+
+	    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
